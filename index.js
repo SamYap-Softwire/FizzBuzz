13995737f554c3842272af3a17252f9a76b000e5
Sam Yun Yap <SamYun.Yap@softwire.com>
2023-06-27 10:27:39 +0100

    Saving Progress - EnableRules still being implemented

--- index.js
+++ index.js
@@ -15,56 +15,6 @@ function BasicFizzBuzz() {
     }
 }
 
-// function AdvancedFizzBuzz(maxNumber){
-//     console.assert(maxNumber >= 1, "Number provided is not valid.");
-//     for (let number = 1; number <= maxNumber; number++){
-//         console.log(AdvancedRules(number));
-//     }
-// }
-
-// function AdvancedRules(number){
-//     let outputMessage = '';
-//     if (number % 3 === 0){
-//         outputMessage = 'Fizz';
-//     }
-//     if (number % 5 === 0){
-//         outputMessage += 'Buzz';
-//     }
-//     if (number % 7 === 0){
-//         outputMessage += 'Bang';
-//     }
-//     if (number % 11 === 0){
-//         outputMessage = 'Bong';
-//     }
-//     if (number % 13 === 0){
-//         let indexOfB = outputMessage.indexOf('B');
-//         if (indexOfB === -1){
-//             outputMessage += 'Fezz';
-//         } else {
-//             outputMessage = outputMessage.substring(0,indexOfB) + 'Fezz' + outputMessage.substring(indexOfB);
-//         }
-//     }
-//     if (number % 17 === 0){
-//         let temporaryMessage = '';
-//         for (let index = 0; index < outputMessage.length; index += 4){
-//             temporaryMessage = outputMessage.substring(index, index+4) + temporaryMessage;
-//         }
-//         outputMessage = temporaryMessage;
-//     }
-//     if (outputMessage === ''){
-//         return number;
-//     }
-//     return outputMessage;
-// }
-
-function EnableRules(userInput){
-    /*User input will be taken in as string
-    * Need to parse the user input and convert into array
-    * Check that array elements are type int
-    * Then sort it in ascending order + filter out numbers that are not valid rules
-    * Then return a function which applies the correct rules*/
-}
-
 function AdvancedFizzBuzz(maxNumber){
     /*maxNumber should be turned into an integer
     * Perform checks on maxNumber
@@ -72,6 +22,19 @@ function AdvancedFizzBuzz(maxNumber){
     * do a for loop
     * perform the stored function in each iteration*/
 }
+
+function EnableRules(userInput){
+    /*User input will be taken in as string
+    * Need to parse the user input and convert into array
+    * Check that array elements are type int
+    * Then sort it in ascending order + filter out numbers that are not valid rules
+    * Then return a function which applies the correct rules*/
+    const listOfRules = userInput.split(' ');
+    for (const element of userInput) {
+        console.assert(typeof element === 'number');
+    }
+}
+
 
 function RuleFor3(currentMessage, number){
     if (number % 3 === 0){
