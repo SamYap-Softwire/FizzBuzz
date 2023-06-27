31858f4fc04799456d165470f79e262c363cf175
Sam Yun Yap <SamYun.Yap@softwire.com>
2023-06-27 10:05:20 +0100

    Skeleton added for user chosen rules

--- index.js
+++ index.js
@@ -15,46 +15,111 @@ function BasicFizzBuzz() {
     }
 }
 
-function AdvancedFizzBuzz(maxNumber){
-    console.assert(maxNumber >= 1, "Number provided is not valid.");
-    for (let number = 1; number <= maxNumber; number++){
-        console.log(AdvancedRules(number));
-    }
+// function AdvancedFizzBuzz(maxNumber){
+//     console.assert(maxNumber >= 1, "Number provided is not valid.");
+//     for (let number = 1; number <= maxNumber; number++){
+//         console.log(AdvancedRules(number));
+//     }
+// }
+
+// function AdvancedRules(number){
+//     let outputMessage = '';
+//     if (number % 3 === 0){
+//         outputMessage = 'Fizz';
+//     }
+//     if (number % 5 === 0){
+//         outputMessage += 'Buzz';
+//     }
+//     if (number % 7 === 0){
+//         outputMessage += 'Bang';
+//     }
+//     if (number % 11 === 0){
+//         outputMessage = 'Bong';
+//     }
+//     if (number % 13 === 0){
+//         let indexOfB = outputMessage.indexOf('B');
+//         if (indexOfB === -1){
+//             outputMessage += 'Fezz';
+//         } else {
+//             outputMessage = outputMessage.substring(0,indexOfB) + 'Fezz' + outputMessage.substring(indexOfB);
+//         }
+//     }
+//     if (number % 17 === 0){
+//         let temporaryMessage = '';
+//         for (let index = 0; index < outputMessage.length; index += 4){
+//             temporaryMessage = outputMessage.substring(index, index+4) + temporaryMessage;
+//         }
+//         outputMessage = temporaryMessage;
+//     }
+//     if (outputMessage === ''){
+//         return number;
+//     }
+//     return outputMessage;
+// }
+
+function EnableRules(userInput){
+    /*User input will be taken in as string
+    * Need to parse the user input and convert into array
+    * Check that array elements are type int
+    * Then sort it in ascending order + filter out numbers that are not valid rules
+    * Then return a function which applies the correct rules*/
 }
 
-function AdvancedRules(number){
-    let outputMessage = '';
+function AdvancedFizzBuzz(maxNumber){
+    /*maxNumber should be turned into an integer
+    * Perform checks on maxNumber
+    * store function returned by enableRules in a variable
+    * do a for loop
+    * perform the stored function in each iteration*/
+}
+
+function RuleFor3(currentMessage, number){
     if (number % 3 === 0){
-        outputMessage = 'Fizz';
+        currentMessage = 'Fizz';
     }
+    return currentMessage;
+}
+
+function RuleFor5(currentMessage, number){
     if (number % 5 === 0){
-        outputMessage += 'Buzz';
+        currentMessage += 'Buzz';
     }
+    return currentMessage;
+}
+
+function RuleFor7(currentMessage, number){
     if (number % 7 === 0){
-        outputMessage += 'Bang';
+        currentMessage += 'Bang';
     }
+    return currentMessage;
+}
+
+function RuleFor11(currentMessage, number){
     if (number % 11 === 0){
-        outputMessage = 'Bong';
+        currentMessage = 'Bong';
     }
-    if (number % 13 === 0){
-        let indexOfB = outputMessage.indexOf('B');
-        if (indexOfB === -1){
-            outputMessage += 'Fezz';
+    return currentMessage;
+}
+
+function RuleFor13(currentMessage, number) {
+    if (number % 13 === 0) {
+        let indexOfB = currentMessage.indexOf('B');
+        if (indexOfB === -1) {
+            currentMessage += 'Fezz';
         } else {
-            outputMessage = outputMessage.substring(0,indexOfB) + 'Fezz' + outputMessage.substring(indexOfB);
+            currentMessage = currentMessage.substring(0, indexOfB) + 'Fezz' + currentMessage.substring(indexOfB);
         }
     }
+}
+function RuleFor17(currentMessage, number){
     if (number % 17 === 0){
         let temporaryMessage = '';
-        for (let index = 0; index < outputMessage.length; index += 4){
-            temporaryMessage = outputMessage.substring(index, index+4) + temporaryMessage;
+        for (let index = 0; index < currentMessage.length; index += 4){
+            temporaryMessage = currentMessage.substring(index, index+4) + temporaryMessage;
         }
-        outputMessage = temporaryMessage;
+        currentMessage = temporaryMessage;
     }
-    if (outputMessage === ''){
-        return number;
-    }
-    return outputMessage;
+    return currentMessage;
 }
 
 /* Main */
