776acf7a2d32961e4c8c1c158a129433cc3f89fe
Sam Yun Yap <SamYun.Yap@softwire.com>
2023-06-28 09:24:05 +0100

    Working code for letting user choose rules - using if else

--- index.js
+++ index.js
@@ -15,77 +15,51 @@ function BasicFizzBuzz() {
     }
 }
 
-function AdvancedFizzBuzz(maxNumber){
-    /*maxNumber should be turned into an integer
-    * Perform checks on maxNumber
-    * store function returned by enableRules in a variable
-    * do a for loop
-    * perform the stored function in each iteration*/
-}
-
-function EnableRules(userInput){
-    /*User input will be taken in as string
-    * Need to parse the user input and convert into array
-    * Check that array elements are type int
-    * Then sort it in ascending order + filter out numbers that are not valid rules
-    * Then return a function which applies the correct rules*/
-    const listOfRules = userInput.split(' ');
-    for (const element of userInput) {
-        console.assert(typeof element === 'number');
+function AdvancedFizzBuzz(maxNumber, userRules){
+    console.assert(maxNumber >= 1, "Number provided is not valid.");
+    for (let number = 1; number <= maxNumber; number++){
+        console.log(AdvancedRules(number, userRules));
     }
 }
 
-
-function RuleFor3(currentMessage, number){
-    if (number % 3 === 0){
-        currentMessage = 'Fizz';
+function AdvancedRules(number, userRules){
+    let outputMessage = '';
+    if (userRules.includes('3') && number % 3 === 0){
+        outputMessage = 'Fizz';
     }
-    return currentMessage;
-}
-
-function RuleFor5(currentMessage, number){
-    if (number % 5 === 0){
-        currentMessage += 'Buzz';
+    if (userRules.includes('5') && number % 5 === 0){
+        outputMessage += 'Buzz';
     }
-    return currentMessage;
-}
-
-function RuleFor7(currentMessage, number){
-    if (number % 7 === 0){
-        currentMessage += 'Bang';
+    if (userRules.includes('7') && number % 7 === 0){
+        outputMessage += 'Bang';
     }
-    return currentMessage;
-}
-
-function RuleFor11(currentMessage, number){
-    if (number % 11 === 0){
-        currentMessage = 'Bong';
+    if (userRules.includes('11') && number % 11 === 0){
+        outputMessage = 'Bong';
     }
-    return currentMessage;
-}
-
-function RuleFor13(currentMessage, number) {
-    if (number % 13 === 0) {
-        let indexOfB = currentMessage.indexOf('B');
-        if (indexOfB === -1) {
-            currentMessage += 'Fezz';
+    if (userRules.includes('13') && number % 13 === 0){
+        let indexOfB = outputMessage.indexOf('B');
+        if (indexOfB === -1){
+            outputMessage += 'Fezz';
         } else {
-            currentMessage = currentMessage.substring(0, indexOfB) + 'Fezz' + currentMessage.substring(indexOfB);
+            outputMessage = outputMessage.substring(0,indexOfB) + 'Fezz' + outputMessage.substring(indexOfB);
         }
     }
-}
-function RuleFor17(currentMessage, number){
-    if (number % 17 === 0){
+    if (userRules.includes('17') && number % 17 === 0){
         let temporaryMessage = '';
-        for (let index = 0; index < currentMessage.length; index += 4){
-            temporaryMessage = currentMessage.substring(index, index+4) + temporaryMessage;
+        for (let index = 0; index < outputMessage.length; index += 4){
+            temporaryMessage = outputMessage.substring(index, index+4) + temporaryMessage;
         }
-        currentMessage = temporaryMessage;
+        outputMessage = temporaryMessage;
     }
-    return currentMessage;
+    if (outputMessage === ''){
+        return number;
+    }
+    return outputMessage;
 }
 
 /* Main */
 
 const maxNumber = prompt("Please enter the number you want to count to: ");
-AdvancedFizzBuzz(maxNumber);
+const userRules = prompt("Please enter the rules you want to include: ").split(' ');
+console.log(userRules);
+AdvancedFizzBuzz(maxNumber, userRules);
